b9eabcb958579f2eed8b6712d440ca3661a43744
csreindeer <channahtrimble@gmail.com>
2018-07-29 19:59:03 -0400

    Update qpuos.py

--- OS/qpuos.py
+++ OS/qpuos.py
@@ -1,7 +1,14 @@
-from qiskit import *
+from pyquil import *
 import init
 import qpuroot
 import qpustandard
+import interface
 
 def os():
-    
+    runroot = True
+    runstand = True
+    while runroot == True:
+        qpuroot.root()
+
+    while runstand == True:
+        qpustandard.standard()
