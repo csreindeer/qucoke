617e5c702f48d9463b608bbfb692bc5d9577c57f
csreindeer <channahtrimble@gmail.com>
2018-07-29 19:58:29 -0400

    Update init.py

--- init.py
+++ init.py
@@ -1,15 +1,11 @@
-from qiskit import *
+from pyquil import *
 import qpuos
 import qpuroot
 import qpustandard
 
 #INIT
-qr = QuantumRegister(2)
-qs = QuantumRegister(2)
-qo = QuantumRegister(3)
-c = ClassicalRegister(1)
-memreg = QuantumRegister(2)
-circuit = QuantumCircuit(qo, qr, qs, c)
+qvm = QVMConnection()
+p = Program()
 
 pswd = "root"; #CHANGE LE PASSWORD
 
