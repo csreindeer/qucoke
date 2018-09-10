0f7f6567aecb69784cf08f98c030d1c63841db00
csreindeer <channahtrimble@gmail.com>
2018-09-10 16:24:45 -0400

    started ascii thing

---
+++ OS/applications/communication/client.py
@@ -0,0 +1,32 @@
+from pyquil.quil import Program
+from pyquil.api import QVMConnection
+from pyquil.gates import CNOT, H
+
+#making ascii like thing, in the works
+def communication():
+    a = 
+    b = 
+    c = 
+    d = 
+    e = 
+    f = 
+    g = 
+    h = 
+    i = 
+    j = 
+    k = 
+    l = 
+    m = 
+    n = 
+    o = 
+    p = 
+    q = 
+    r = 
+    s = 
+    t = 
+    u = 
+    v = 
+    w = 
+    x = 
+    y = 
+    z =  
